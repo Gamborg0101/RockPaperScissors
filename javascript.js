9e58877d0cfc8475b27788030da1be10d04a2122
Casper Gamborg <Gamborgc@gmail.com>
2025-03-04 09:19:04 +0100

    Logic is almost done - just need to adjust round counter

--- javascript.js
+++ javascript.js
@@ -14,22 +14,22 @@ let playRound = (getHumanChoice, getComputerChoice) => {
   let humanWins = "Human wins this round!";
   let computerWins = "Computer wins this round";
   //Higher order functions
-  let humanChoice = getHumanChoice().toLowerCase();
-  let computerChoice = getComputerChoice();
-  
-  if (humanChoice == "") {
-    console.log("You need to enter something");
-  } else if (
-    humanChoice !== "rock" &&
-    humanChoice !== "paper" &&
-    humanChoice !== "scissors"
-  ) {
-    console.log("Invalid choice");
-  }
+  for (let n = 0; n < 5; n++) {
+    let humanChoice = getHumanChoice().toLowerCase();
+    let computerChoice = getComputerChoice();
 
-  for (let n = 5; n <= 5; n++) {
+    if (humanChoice == "") {
+      console.log("You need to enter something");
+    } else if (
+      humanChoice !== "rock" &&
+      humanChoice !== "paper" &&
+      humanChoice !== "scissors"
+    ) {
+      console.log("Invalid choice");
+    }
+
     if (humanChoice == computerChoice) {
-      return console.log("Its a tie!");
+      console.log("Its a tie!");
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
       console.log(humanWins);
       humanScore++;
@@ -48,7 +48,6 @@ let playRound = (getHumanChoice, getComputerChoice) => {
     console.log("ComputerScore: " + computerScore);
     console.log("Human: " + humanScore);
   }
-    
 };
 
 //console.log("ComputerChoice = " + playRound(getComputerChoice, getHumanChoice));
