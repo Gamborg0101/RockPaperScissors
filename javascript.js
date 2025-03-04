a51790f4235f12d4c4711d604bda4fb16064f7d6
Casper Gamborg <Gamborgc@gmail.com>
2025-03-04 18:00:21 +0100

    Notes updated

--- javascript.js
+++ javascript.js
@@ -39,50 +39,10 @@ function playSingleRound(humanChoice, computerChoice) {
 
 console.log(playSingleRound(getHumanChoice(), getComputerChoice()));
 
-/*function playGame(humanChoice, computerChoice) {
-
-  //Higher order functions
-
-  function playSingleRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-      console.log("Its a tie!");
-      console.log("Round: " + n); //Could this not just be || statements inside on else if - and then if these are not true, then give the computer the point?
-    } else if (humanChoice == "rock" && computerChoice == "scissors") {
-      console.log("This is round:" + n + " \nThe human won this round");
-      humanScore++;
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-      console.log("This is round:" + n + " \nThe human won this round");
-      humanScore++;
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-      console.log("This is round:" + n + " \nThe human won this round");
-      humanScore++;
-    } else if (humanChoice == "rock" && computerChoice == "paper") {
-      console.log("This is round:" + n + " \nThe computer won this round");
-      computerScore++;
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-      console.log("This is round:" + n + " \nThe computer won this round");
-      computerScore++;
-    } else if (humanChoice == "paper" && computerChoice == "scissors") {
-      console.log("This is round:" + n + " \nThe computer won this round");
-      computerScore++;
-    } else {
-      console.log("Something went wrong!");
-    }
-    console.log("ComputerScore: " + computerScore);
-    console.log("Human: " + humanScore);
-  }
-  console.log("Total score for the computer: " + computerScore);
-  console.log("Total score for the human: " + humanScore);
-
-  if (computerScore > humanScore) {
-    console.log("The computer wins!");
-  } else if (computerScore < humanScore) {
-    console.log("The human wins!");
-  }
-
-  for (let n = 1; n <= 5; n++) {
-    playSingleRound(humanChoice, computerChoice);
-  }
-}
-
-playGame(getHumanChoice(), getComputerChoice());*/
+/* 
+Iterer over funktionen
+Resultatet fra funktionen skal være hvem der vinder. 
+Derefter plus score - playGame () skal holde styr på scoren. 
+Skal playGame tage et argument med antal runder, som også skal være en del af prompten. 
+Ud fra score kan vi definer vinder. 
+*/
