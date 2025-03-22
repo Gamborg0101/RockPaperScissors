3f6da1624cced510606a672fd7c703bf878fdc11
Casper Gamborg <Gamborgc@gmail.com>
2025-03-22 09:05:16 +0100

    Need to run by Niels for clearing up logic. Proposals for optimizations are written in bottom of js file.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 let button = document.getElementById("buttons");
+let winnerPlacement = document.querySelector(".winner");
 
 button.addEventListener("click", function (e) {
   let humanClick = e.target.value;
@@ -13,25 +14,37 @@ let getComputerChoice = () => {
   return choices[Math.floor(Math.random() * 3)];
 };
 
-function playSingleRound(humanChoice, computerChoice) {
-  let winnerPlacement = document.querySelector(".winner");
+function winnerAnnouncement(message) {
+  let messageDiv = document.createElement("div");
+  messageDiv.classList.add("winnerdiv");
+  messageDiv.innerHTML = message;
+  messageDiv.style.fontSize = "32px";
+  messageDiv.style.fontWeight = "bold";
+
+  winnerPlacement.appendChild(messageDiv);
   winnerPlacement.classList.add("winnerplacement");
+  winnerPlacement.style.display = "flex";
+  winnerPlacement.style.flexDirection = "column";
+  return;
+}
+
+function playSingleRound(humanChoice, computerChoice) {
   winnerPlacement.textContent = "";
 
   let humanWinsDiv = document.createElement("div");
   humanWinsDiv.classList.add("humanwinsdiv");
 
-  if (humanScore == 5) {
-    winnerAnnouncement(
-      `Human wins! <br> Humanscore: ${humanScore} <br> Computerscore: ${computerScore} <br> Congrats! <br>`
-    );
-    return;
-  } else if (computerScore == 5) {
-    winnerAnnouncement(
-      `Computer wins! <br> Humanscore: ${humanScore} <br> Computerscore: ${computerScore} <br> Congrats! <br>`
-    );
-    return;
-  }
+  // if (humanScore == 5) {
+  //   winnerAnnouncement(
+  //     `Human wins! <br> Humanscore: ${humanScore} <br> Computerscore: ${computerScore} <br> Congrats! <br>`
+  //   );
+  //   return;
+  // } else if (computerScore == 5) {
+  //   winnerAnnouncement(
+  //     `Computer wins! <br> Humanscore: ${humanScore} <br> Computerscore: ${computerScore} <br> Congrats! <br>`
+  //   );
+  //   return;
+  // }
 
   if (humanChoice == computerChoice) {
     humanWinsDiv.innerHTML = `It's a tie! <br> HumanScore: ${humanScore} <br> Computerscore: ${computerScore}`;
@@ -45,23 +58,32 @@ function playSingleRound(humanChoice, computerChoice) {
     (humanChoice == "scissors" && computerChoice == "paper")
   ) {
     humanScore++;
-    humanWinsDiv.innerHTML = `Human wins! <br> HumanScore: ${humanScore} <br> Computerscore: ${computerScore}`;
-    winnerPlacement.appendChild(humanWinsDiv);
+  } else {
+    computerScore++;
+  }
+
+  if (humanScore == 5) {
+    winnerAnnouncement(
+      `Human wins! <br> Humanscore: ${humanScore} <br> Computerscore: ${computerScore} <br> Congrats! <br>`
+    );
+    return;
+  } else if (computerScore == 5) {
+    winnerAnnouncement(
+      `Computer wins! <br> Humanscore: ${humanScore} <br> Computerscore: ${computerScore} <br> Congrats! <br>`
+    );
     return;
   }
-
-  computerScore++;
-  humanWinsDiv.innerHTML = `Computer wins! <br> HumanScore: ${humanScore} <br> Computerscore: ${computerScore}`;
+  humanWinsDiv.innerHTML = `HumanScore: ${humanScore} <br> Computerscore: ${computerScore}`;
   winnerPlacement.appendChild(humanWinsDiv);
-  return;
-
-  function winnerAnnouncement(message) {
-    let messageDiv = document.createElement("h1");
-    messageDiv.classList.add("winnerdiv");
-    messageDiv.innerHTML = message;
-
-    winnerPlacement.appendChild(messageDiv);
-    winnerPlacement.style.display = "flex";
-    winnerPlacement.style.flexDirection = "column";
-  }
 }
+
+/*
+Optimeringsforslag: 
+* Jeg kan lave en global pointcounter samt en maxScore == 5;
+* Så ville jeg også kunne lave et pointcheck efter hver runde i starten af RunGame-funktionen der kører alle funktionerne. 
+* Jeg kunne lave en runRound function, som kører spillogikken - bestemmer vinder og taber. Hvis computer vinder -> winner(arg1) && loser(arg1)
+* Winner / loser funktion. Kunne gøre det mere læsbart.
+* Jeg kunne lave winnerstyle og loserstyle til individuelle funktioner selv. Seperation of responsibility. 
+* Generelt er der en masse variabelnavne som kunne optimeres for løsbarheden. 
+
+*/
